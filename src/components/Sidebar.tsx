5d60e0f71bd25d6786c3616aeebb619d6eb8e56e
s-tanthikun802 <s.tanthikun802@mybvc.ca>
2024-04-03 15:23:23 -0600

    feat: add avatar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { List, ListItem, Text } from "@chakra-ui/react";
+import { Avatar, Flex, List, ListItem, Text } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
 
 function Sidebar() {
@@ -15,29 +15,36 @@ function Sidebar() {
   };
 
   return (
-    <List color={"white"} fontSize={"1.2em"} fontWeight={"bold"} spacing={10}>
-      <ListItem>
-        <Text fontSize={"2em"}>My Hospital</Text>
-      </ListItem>
-      <ListItem>
-        <NavLink
-          to={"/"}
-          onMouseOver={handleMouseOver}
-          onMouseOut={handleMouseOut}
-        >
-          Dashboard
-        </NavLink>
-      </ListItem>
-      <ListItem>
-        <NavLink
-          to={"/medical-record"}
-          onMouseOver={handleMouseOver}
-          onMouseOut={handleMouseOut}
-        >
-          Medical Record
-        </NavLink>
-      </ListItem>
-    </List>
+    <Flex
+      flexDirection={"column"}
+      justifyContent={"space-between"}
+      height={"100%"}
+    >
+      <List color={"white"} fontSize={"1.2em"} fontWeight={"bold"} spacing={10}>
+        <ListItem>
+          <Text fontSize={"2em"}>My Hospital</Text>
+        </ListItem>
+        <ListItem>
+          <NavLink
+            to={"/"}
+            onMouseOver={handleMouseOver}
+            onMouseOut={handleMouseOut}
+          >
+            Dashboard
+          </NavLink>
+        </ListItem>
+        <ListItem>
+          <NavLink
+            to={"/medical-record"}
+            onMouseOver={handleMouseOver}
+            onMouseOut={handleMouseOut}
+          >
+            Medical Record
+          </NavLink>
+        </ListItem>
+      </List>
+      <Avatar size={"md"} name={"S"} />
+    </Flex>
   );
 }
 
