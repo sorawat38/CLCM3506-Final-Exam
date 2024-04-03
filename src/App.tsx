e2cc3dcc76148c9118ed3a782d626e754544d7df
s-tanthikun802 <s.tanthikun802@mybvc.ca>
2024-04-03 12:14:59 -0600

    style: reformat style

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import './App.css'
+import "./App.css";
 import HomePage from "./pages/HomePage.tsx";
 import MedicalRecord from "./pages/MedicalRecord.tsx";
 
 function App() {
-
   return (
     <>
       <HomePage />
-        <MedicalRecord />
+      <MedicalRecord />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
