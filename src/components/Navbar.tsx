8727a60853708eef6c505b570e5c73d349ca02ee
s-tanthikun802 <s.tanthikun802@mybvc.ca>
2024-04-03 14:48:03 -0600

    feat: create root layout

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
+
+function Navbar() {
+  return (
+    <Flex as={"nav"} alignItems={"center"}>
+      <Heading as={"h1"}>Hello, Sorawat</Heading>
+      <Spacer />
+      <HStack spacing={"20px"}>
+        <Box bg={"gray.200"} p={"10px"}>
+          M
+        </Box>
+        <Text>test@test.com</Text>
+        <Button colorScheme={"yellow"}>Logout</Button>
+      </HStack>
+    </Flex>
+  );
+}
+
+export default Navbar;
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,10 +1,29 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar.tsx";
+import { Flex, Grid, GridItem } from "@chakra-ui/react";
 
 function RootLayout() {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" "header" "main"`,
+        md: `"nav header" "nav main"`,
+      }}
+      gridTemplateRows={"auto 1fr"}
+      gridTemplateColumns={{ base: "1fr", md: "1fr 5fr" }}
+    >
+      <GridItem bg={"teal.300"} minHeight={{ md: "100vh" }} area={"nav"}>
+        <Flex justifyContent="center" pt={"50px"}>
+          <span>sidebar</span>
+        </Flex>
+      </GridItem>
+      <GridItem p={"40px"} area={"header"}>
+        <Navbar />
+      </GridItem>
+      <GridItem p={"40px"} area={"main"}>
+        <Outlet />
+      </GridItem>
+    </Grid>
   );
 }
 
