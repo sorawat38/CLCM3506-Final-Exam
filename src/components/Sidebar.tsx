19eb940bd64973d6dd150791ae717d2e3d6336e0
s-tanthikun802 <s.tanthikun802@mybvc.ca>
2024-04-03 15:11:17 -0600

    feat: add style

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,16 +2,40 @@ import { List, ListItem, Text } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
 
 function Sidebar() {
+  const handleMouseOver = (e: {
+    currentTarget: { style: { textDecoration: string } };
+  }) => {
+    e.currentTarget.style.textDecoration = "underline";
+  };
+
+  const handleMouseOut = (e: {
+    currentTarget: { style: { textDecoration: string } };
+  }) => {
+    e.currentTarget.style.textDecoration = "none";
+  };
+
   return (
     <List color={"white"} fontSize={"1.2em"} fontWeight={"bold"} spacing={10}>
       <ListItem>
         <Text fontSize={"2em"}>My Hospital</Text>
       </ListItem>
       <ListItem>
-        <NavLink to={"/"}>Dashboard</NavLink>
+        <NavLink
+          to={"/"}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          Dashboard
+        </NavLink>
       </ListItem>
       <ListItem>
-        <NavLink to={"/medical-record"}>Medical Record</NavLink>
+        <NavLink
+          to={"/medical-record"}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          Medical Record
+        </NavLink>
       </ListItem>
     </List>
   );
