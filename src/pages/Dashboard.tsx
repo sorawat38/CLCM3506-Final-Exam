1eb5dbc07b5788dffe8d10623c9e0e12083b1ff3
s-tanthikun802 <s.tanthikun802@mybvc.ca>
2024-04-03 16:37:10 -0600

    feat: add dashboard tempalte

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,45 @@
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Grid,
+  GridItem,
+  Heading,
+} from "@chakra-ui/react";
+
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <Grid
+      templateRows={"repeat(2, 1fr)"}
+      templateColumns={"repeat(2, 1fr)"}
+      gap={10}
+    >
+      <GridItem colSpan={2}>
+        <Card>
+          <CardHeader>
+            <Heading size={"lg"}>Upcoming appointments</Heading>
+          </CardHeader>
+          <CardBody></CardBody>
+        </Card>
+      </GridItem>
+      <GridItem colSpan={1}>
+        <Card>
+          <CardHeader>
+            <Heading size={"lg"}>Next meetings</Heading>
+          </CardHeader>
+          <CardBody></CardBody>
+        </Card>
+      </GridItem>
+      <GridItem colSpan={1}>
+        <Card>
+          <CardHeader>
+            <Heading size={"lg"}>Lab and Test Result</Heading>
+          </CardHeader>
+          <CardBody></CardBody>
+        </Card>
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default Dashboard;
