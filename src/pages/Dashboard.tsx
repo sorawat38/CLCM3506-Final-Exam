ea76a6b520a0e39dcb3e8827b1e3d7b1cfe85910
s-tanthikun802 <s.tanthikun802@mybvc.ca>
2024-04-03 17:34:59 -0600

    feat: complete upcoming appointments

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,13 @@
 import {
+  Button,
   Card,
   CardBody,
   CardHeader,
+  Flex,
   Grid,
   GridItem,
   Heading,
+  Image,
 } from "@chakra-ui/react";
 
 function Dashboard() {
@@ -15,7 +18,7 @@ function Dashboard() {
       gap={10}
     >
       <GridItem colSpan={2}>
-        <Card>
+        <Card overflow="hidden">
           <CardHeader
             borderTopRadius={"lg"}
             bgGradient={"linear(to-r, teal.400, teal.200)"}
@@ -24,7 +27,36 @@ function Dashboard() {
               Upcoming appointments
             </Heading>
           </CardHeader>
-          <CardBody></CardBody>
+          <Grid templateColumns={"auto 1fr 1fr"}>
+            <Image
+              borderRadius="full"
+              maxW={{ base: "100%", md: "300px" }}
+              src="src/assets/woman_icon.png"
+              alt="Caffe Latte"
+              p={"20px"}
+            />
+            <CardBody>
+              <Heading size={"xl"}>Sophia Reynolds</Heading>
+              <Heading size={"md"} color={"teal.500"}>
+                10:00 AM - 11:00 AM
+              </Heading>
+            </CardBody>
+            <Flex
+              direction={"row"}
+              height={"100%"}
+              justifyContent={"flex-end"}
+              alignItems={"flex-end"}
+              gap={4}
+              p={"20px"}
+            >
+              <Button colorScheme="teal" flexGrow={2} size={"lg"}>
+                Join
+              </Button>
+              <Button colorScheme="red" flexGrow={2} size={"lg"}>
+                Cancel
+              </Button>
+            </Flex>
+          </Grid>
         </Card>
       </GridItem>
       <GridItem colSpan={1}>
